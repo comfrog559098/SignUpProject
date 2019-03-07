20b8f7f60b7024e3c2ebde4d4ce6ae978584363a
Jon Latimer <jon.latimer@mymhc.ca>
2019-03-07 09:48:58 -0700

    added some shit

--- SignUpProject/Contact.aspx.cs
+++ SignUpProject/Contact.aspx.cs
@@ -9,6 +9,15 @@ public partial class Contact : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if(Application["host"].ToString()=="browserbytes")
+        {
+            Label1.Text = "Ｐｈｏｎｅ：(403)111-1111";
+            Label2.Text = "Ξｍａｉｌ：browserbytemhc@gmail.com ";
+        }
+        else if (Application["host"].ToString() == "surgeprotectors")
+        {
+            Label1.Text = "Ｐｈｏｎｅ：(403)866-7041 ";
+            Label2.Text = "Ξｍａｉｌ： thesurgeprotectors22@gmail.com ";
+        }
     }
 }
